c56fec2952f06462f8ec8d57cba49be26b909ccc
benzps01 <benzps01@gmail.com>
2024-04-07 00:10:00 +0530

    removed wanted codes

--- assignments_nilesh_sir/java_assignments/Assignment_1/test.java
+++ assignments_nilesh_sir/java_assignments/Assignment_1/test.java
@@ -1,8 +1,6 @@
 public class test {
     public static void main(String[] args) {
 
-        // MathUtil m1 = new MathUtil();
-
         System.out.println("No. of primes are: " + MathUtil.countPrime(2, 20));
         System.out.println("Addition is: " + MathUtil.add(23, 26));
         System.out.println("Subtraction: " + MathUtil.subtract(25, 15));

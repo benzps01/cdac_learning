bf13314654df940289072f6ceb363f2c1f3437c7
benzps01 <benzps01@gmail.com>
2024-04-13 06:33:24 +0530

    added items to cart and handled exception for deleteItem

--- assignments_nilesh_sir/java_assignments/Assignment_5/ProgramTest.java
+++ assignments_nilesh_sir/java_assignments/Assignment_5/ProgramTest.java
@@ -1,18 +1,48 @@
+import java.util.Scanner;
+
 public class ProgramTest {
     public static void main(String[] args) {
-        ShoppingCart s1 = new ShoppingCart();
+        Scanner cartObj = new Scanner(System.in);
 
-        for (int i = 1; i < 6; i++) {
-            System.out.println(s1.printItem(i));
+        ShoppingCart cart = new ShoppingCart();
+
+        System.out.println("Enter the Cart Size: ");
+        int cartSize = cartObj.nextInt();
+        cartObj.nextLine();
+
+        try {
+            for (int i = 1; i <= cartSize; i++) {
+                System.out.println("Enter the CostPerUnit");
+                int cp = cartObj.nextInt();
+                System.out.println();
+                System.out.println("Enter the Quantity");
+                int quant = cartObj.nextInt();
+                System.out.println();
+                cart.addItem(new Item(cp, quant));
+            }
+
+            for (int i = 1; i <= cartSize; i++) {
+                System.out.println(cart.printItem(i));
+            }
+
+            System.out.println("Total Cart Price: " + cart.cartPrice());
+            System.out.println();
+
+            try {
+                System.out.println("Enter Id to be deleted: ");
+                int id = cartObj.nextInt();
+                cart.deleteItem(id);
+            } catch (ItemDoesntExistException e) {
+                System.out.println("There is no such Item. Check your ID!!!!");
+                System.out.println();
+            }
+
+            for (int i = 1; i <= cartSize; i++) {
+                System.out.println(cart.printItem(i));
+            }
+        } finally {
+            cartObj.close();
         }
-
-        System.out.println("Total Cart Price: " + s1.cartPrice());
-
-        for (int i = 1; i < 6; i++) {
-            System.out.println(s1.printItem(i));
-        }
-
-        // s1.deleteItem(2);
 
     }
 }

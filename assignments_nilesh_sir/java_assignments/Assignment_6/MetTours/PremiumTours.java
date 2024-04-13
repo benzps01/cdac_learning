ef64259e15c854cb9a206a5fab45091c10510f99
benzps01 <benzps01@gmail.com>
2024-04-13 07:01:18 +0530

    removed discount to TotalCost,  resulting discount being added to Economy too

--- assignments_nilesh_sir/java_assignments/Assignment_6/MetTours/PremiumTours.java
+++ assignments_nilesh_sir/java_assignments/Assignment_6/MetTours/PremiumTours.java
@@ -1,14 +1,15 @@
 package MetTours;
 
 public class PremiumTours {
-
+    static int discount = 0;
     /*
      * Here we calculate PerDayRent depending on the no of Persons.
      */
 
     @LuxuryTax(value = 10)
     public double getPerDayRentForCommon(int days, int noPersons) {
-        return noPersons * 1200;
+        discount += (days > 6 ? days * 100 : 0) + (noPersons >= 4 ? noPersons * 100 : 0);
+        return (noPersons * 1200) - discount;
     }
 
     @LuxuryTax(value = 10)
--- assignments_nilesh_sir/java_assignments/Assignment_6/MetTours/TotalCost.java
+++ assignments_nilesh_sir/java_assignments/Assignment_6/MetTours/TotalCost.java
@@ -1,16 +1,12 @@
 package MetTours;
 
 public class TotalCost {
-    static int discount = 0;
 
     /*
-     * Here we calculate TotalCost using costPerDay and get discount depending
-     * on the number of days and noPersons.
+     * Here we calculate TotalCost using costPerDay.s
      */
     public static double totalCost(double costPerDay, int days, int noPersons) {
-        discount += (days > 6 ? days * 100 : 0) + (noPersons >= 4 ? noPersons * 100 : 0);
-        costPerDay *= days;
-        return costPerDay -= discount;
+        return costPerDay *= days;
 
     }
 }
